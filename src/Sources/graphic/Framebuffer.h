cfd2c4c7a8d1839baa415f283f3b5fe1ab27ee4d
Yiwei Gong <imwithye@gmail.com>
2019-10-13 02:37:58 +0800

    add basic resources

---
+++ src/Sources/graphic/Framebuffer.h
@@ -0,0 +1,10 @@
+// Copyright 2019
+#pragma once
+
+#include "Component.h"
+
+namespace lucyrt {
+namespace graphic {
+class Framebuffer {};
+}  // namespace graphic
+}  // namespace lucyrt
---
+++ src/Sources/graphic/Texture.h
@@ -0,0 +1,10 @@
+// Copyright 2019
+#pragma once
+
+#include "Component.h"
+
+namespace lucyrt {
+namespace graphic {
+class Texture {};
+}  // namespace graphic
+}  // namespace lucyrt
