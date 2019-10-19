984f4d7e7bb6de5740189a340b252461ee580ed3
Yiwei Gong <imwithye@gmail.com>
2019-10-20 02:17:08 +0800

    Texture Data library stores data only

--- src/Sources/graphic/Texture.cc
+++ src/Sources/graphic/Texture.cc
@@ -6,44 +6,20 @@
 #include <iostream>
 
 using lucyrt::graphic::Texture;
+using lucyrt::graphic::TextureData;
 using lucyrt::graphic::TexturePtr;
 
 #define C(x) static_cast<GLbyte>(x)
 
-std::unordered_map<std::string, TexturePtr*> Texture::library_;
+TextureData::TextureData() : width_(0), height_(0) {}
 
-TexturePtr Texture::Load(const std::string& filepath) {
-  if (library_.find(filepath) == library_.end()) {
-    TexturePtr ptr = std::shared_ptr<Texture>(new Texture(filepath), Delete);
-    library_[filepath] = &ptr;
-    return ptr;
-  }
-  return *library_[filepath];
-}
-
-void Texture::Delete(Texture* texture) {
-  library_.erase(texture->filepath);
-  delete texture;
-}
-
-void Texture::Active(GLenum unit) {
-  glActiveTexture(unit);
-  glBindTexture(GL_TEXTURE_2D, id_);
-}
-
-Texture::~Texture() {
-  glDeleteTextures(1, &id_);
-  spdlog::trace("{} deleted", this);
-}
-
-Texture::Texture(const std::string& filepath)
-    : filepath(filepath), id_(0), width_(0), height_(0) {
+TextureData::TextureData(const std::string& filepath) {
   stbi_set_flip_vertically_on_load(true);
   int width = 0, height = 0, channels = 0;
   unsigned char* data =
       stbi_load(filepath.c_str(), &width, &height, &channels, 4);
   if (!data) {
-    spdlog::error("{} failed to load from {}", this, filepath);
+    spdlog::error("TextureData failed to load from {}", filepath);
     width_ = height_ = 2;
     data_.resize(width_ * height_ * 4);
     data_[0] = 0, data_[1] = 0, data_[2] = 0, data_[3] = C(255);
@@ -57,6 +33,34 @@ Texture::Texture(const std::string& filepath)
     std::copy(data, data + data_.size(), data_.begin());
     stbi_image_free(data);
   }
+  spdlog::trace("TextureData loaded from {}", filepath);
+}
+
+TextureData::TextureData(const TextureData& t)
+    : data_(t.data_), width_(t.width_), height_(t.height_) {}
+
+std::unordered_map<std::string, TextureData> Texture::library_;
+
+TexturePtr Texture::Load(const std::string& filepath) {
+  if (library_.find(filepath) == library_.end()) {
+    library_[filepath] = TextureData(filepath);
+    TexturePtr ptr(new Texture(filepath, library_[filepath]), Delete);
+    return ptr;
+  } else {
+    TexturePtr ptr(new Texture(filepath, library_[filepath]), Delete);
+    return ptr;
+  }
+}
+
+void Texture::Delete(Texture* texture) { delete texture; }
+
+void Texture::Active(GLenum unit) {
+  glActiveTexture(unit);
+  glBindTexture(GL_TEXTURE_2D, id_);
+}
+
+Texture::Texture(const std::string& name, const TextureData& data)
+    : name(name) {
   glGenTextures(1, &id_);
   glBindTexture(GL_TEXTURE_2D, id_);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
@@ -65,8 +69,14 @@ Texture::Texture(const std::string& filepath)
                   GL_LINEAR_MIPMAP_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER,
                   GL_LINEAR_MIPMAP_LINEAR);
-  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width_, height_, 0, GL_RGBA,
-               GL_UNSIGNED_BYTE, reinterpret_cast<void*>(data_.data()));
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, data.width_, data.height_, 0, GL_RGBA,
+               GL_UNSIGNED_BYTE,
+               reinterpret_cast<const void*>(data.data_.data()));
   glGenerateMipmap(GL_TEXTURE_2D);
   spdlog::trace("{} initialized", this);
+}
+
+Texture::~Texture() {
+  glDeleteTextures(1, &id_);
+  spdlog::trace("{} deleted", this);
 }
--- src/Sources/graphic/Texture.h
+++ src/Sources/graphic/Texture.h
@@ -15,31 +15,42 @@ namespace graphic {
 class Texture;
 typedef std::shared_ptr<Texture> TexturePtr;
 
+class TextureData {
+ public:
+  TextureData();
+  explicit TextureData(const std::string& filepath);
+  TextureData(const TextureData& t);
+
+ private:
+  std::vector<GLbyte> data_;
+  GLuint width_;
+  GLuint height_;
+
+  friend class Texture;
+};
+
 class Texture {
  public:
-  std::string filepath;
+  std::string name;
 
   static TexturePtr Load(const std::string& filepath);
   static void Delete(Texture* texture);
 
  private:
-  static std::unordered_map<std::string, TexturePtr*> library_;
-  explicit Texture(const std::string& filepath);
+  static std::unordered_map<std::string, TextureData> library_;
+  Texture(const std::string& name, const TextureData& data);
   ~Texture();
 
  public:
   template <typename OStream>
   friend OStream& operator<<(OStream& os, const Texture* t) {
-    return os << "Texture[" << t->filepath << "(id:" << t->id_ << ")]";
+    return os << "Texture[" << t->name << "(id:" << t->id_ << ")]";
   }
 
   void Active(GLenum uint);
 
  private:
   GLuint id_;
-  GLuint width_;
-  GLuint height_;
-  std::vector<GLbyte> data_;
 };
 }  // namespace graphic
 }  // namespace lucyrt
