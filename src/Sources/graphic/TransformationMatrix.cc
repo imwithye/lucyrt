68843013e6b0a38b33a6ed0dea5a62c5a93d2b43
Yiwei Gong <imwithye@gmail.com>
2019-10-20 21:20:38 +0800

    translation matrix uses col first

--- src/Sources/graphic/TransformationMatrix.cc
+++ src/Sources/graphic/TransformationMatrix.cc
@@ -10,21 +10,21 @@ TransformationMatrix::TransformationMatrix() : matrix(1.0) {}
 TransformationMatrix::TransformationMatrix(glm::mat4 matrix) : matrix(matrix) {}
 
 vec3 TransformationMatrix::GetPos() const {
-  return vec3(matrix[0][3], matrix[1][3], matrix[2][3]);
+  return vec3(matrix[3][0], matrix[3][1], matrix[3][2]);
 }
 
 void TransformationMatrix::SetPos(vec3 pos) {
-  matrix[0][3] = pos[0];
-  matrix[1][3] = pos[1];
-  matrix[2][3] = pos[2];
+  matrix[3][0] = pos[0];
+  matrix[3][1] = pos[1];
+  matrix[3][2] = pos[2];
 }
 
 vec3 TransformationMatrix::GetUp() const {
-  return normalize(vec3(matrix[0][1], matrix[1][1], matrix[2][1]));
+  return normalize(vec3(matrix[1][0], matrix[1][1], matrix[1][2]));
 }
 
 vec3 TransformationMatrix::GetForward() const {
-  return -normalize(vec3(matrix[0][2], matrix[1][2], matrix[2][2]));
+  return -normalize(vec3(matrix[2][0], matrix[2][1], matrix[2][2]));
 }
 
 void TransformationMatrix::SetForward(vec3 forward) {
@@ -37,10 +37,10 @@ void TransformationMatrix::SetForward(vec3 forward) {
   } else if (oldf[2] != 0) {
     axis = 2;
   }
-  float r = vec3(matrix[0][2], matrix[1][2], matrix[2][2])[axis] / oldf[axis];
+  float r = vec3(matrix[2][0], matrix[2][1], matrix[2][2])[axis] / oldf[axis];
   forward = r * normalize(forward);
-  matrix[0][2] = forward[0];
-  matrix[1][2] = forward[1];
+  matrix[2][0] = forward[0];
+  matrix[2][1] = forward[1];
   matrix[2][2] = forward[2];
 }
 
